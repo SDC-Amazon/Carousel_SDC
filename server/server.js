5c3a2a22c7f02b4102b94d4bbdb31ead3c06a2ab
JoelArmendariz <aztemba@gmail.com>
2019-10-24 16:06:39 -0500

    troubleshoot

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const db = require('./db.js')
+const cors = require('cors')
 
 const app = express();
 
 app.use(express.static(__dirname + '/../dist'));
 app.use(express.json());
+app.use(cors());
 
 app.get('/carousel', (req, res) => {
   let id = req.query.id;
